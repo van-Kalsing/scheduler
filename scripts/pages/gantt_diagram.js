aae4b47ec09abd408fb4632ec67decf8285f9424
van_Kalsing <kalsin@inbox.ru>
2014-05-05 17:18:49 +0600

    Исправление ошибки коммита e2eeab92d444ace3c29143b10c61df120f6168f1

--- scripts/pages/gantt_diagram.js
+++ scripts/pages/gantt_diagram.js
@@ -1,7 +1,8 @@
 function render_gantt_diagram(container_element) {
 	container_element.html(
 		"<img id='cap' src='http://image.etov.com.ua/storage/640x640/9/a/b/6/9ab654c3da47e553d45e5341aca176cd.jpg' />"
-	
+  );
+  
 	var width = 960,
     height = 500,
     fill = d3.scale.category20();
@@ -259,6 +260,4 @@ function keydown() {
     }
   }
 }
-	
-	);
 }
