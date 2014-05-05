9f6236a5f416e4916cfd90c38aa49e77c9addbcf
Aleksey <webdesigner91@yandex.ru>
2014-05-05 11:56:06 +0600

    Update gantt_diagram.js

--- scripts/pages/gantt_diagram.js
+++ scripts/pages/gantt_diagram.js
@@ -1,5 +1,5 @@
 function render_gantt_diagram(container_element) {
 	container_element.html(
-		"<img id='cap' src='images/caps/gantt-diagram.png' />"
+		"<img id='cap' src='http://image.etov.com.ua/storage/640x640/9/a/b/6/9ab654c3da47e553d45e5341aca176cd.jpg' />"
 	);
 }
