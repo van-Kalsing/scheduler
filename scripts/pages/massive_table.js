425815d9ef283719dea8d2b70f8978108c761286
van_Kalsing <kalsin@inbox.ru>
2014-06-04 15:42:32 +0600

    Реализация табличного календаря: исправление ошибки коммита dadd3ab3bbaf9abb5f4f20f52afc8c3def745d7f

---
+++ scripts/pages/massive_table.js
@@ -0,0 +1,23 @@
+function render_table_calendar(container_element) {
+	var container_element_id = container_element.attr("id");
+	
+	var grid =
+		[ ["value","value","value","value","value","value","value"],
+		  ["value","value","value","value","value","value","value"],
+		  ["value","value","value","value","value","value","value"],
+		  ["value","value","value","value","value","value","value"],
+		  ["value","value","value","value","value","value","value"],
+		  ["value","value","value","value","value","value","value"],
+		];
+		
+	var table = d3.select("#" + container_element_id).append("table");
+	       // thead = table.append("thead"),
+	      //  tbody = table.append("tbody");
+	for (var i=0;i<6;++i) {
+		var row=table.append ("tr");
+		for (var j=0;j<7;++j) {
+			var cell = row.append("td");
+			cell.text(grid[i][j]);
+		}
+	}
+}
